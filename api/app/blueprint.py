b4a87d3ff5902d457d645272e6b82bc776b0af70
Brian Matheson <bmath@wit.com>
2020-09-25 09:35:20 -0400

    add db connection, routes for db inserts and lookups

--- api/app/blueprint.py
+++ api/app/blueprint.py
@@ -1,13 +1,40 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from flask_api import status
+from psycopg2 import connect
+import json 
 
 blueprint = Blueprint('blueprint', __name__)
+db = connect(host="db-simple-flask-app-postgresql", 
+             dbname="simple_flask_app",
+             user="postgres", 
+             password="rX0l6HqO2p")
 
 @blueprint.route('/ping')
 @blueprint.route('/')
 def ping():
-    return 'ack!'
+    return 'pong!'
 
 @blueprint.route('/hello')
 def hello_world():
     return 'Hello, World!'
 
+@blueprint.route('/echo', methods=["POST"])
+def echo():
+    uuid = request.form['uuid']
+    return(uuid + "\n")
+
+@blueprint.route('/add', methods=["POST"])
+def add():
+    uuid = request.form['uuid']
+    cur = db.cursor()
+    query = "INSERT INTO uuids (uuid) VALUES (%s);"
+    cur.execute(query, [uuid])
+    return "okay\n", status.HTTP_201_CREATED
+
+@blueprint.route('/get', methods=["GET"])
+def get():
+    cur = db.cursor()
+    query = "SELECT * from uuids;"
+    cur.execute(query)
+    resp = json.dumps(cur.fetchall())
+    return(resp)
